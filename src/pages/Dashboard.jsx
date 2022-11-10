44045ba542bd658c0ee5ceae39aba2697ae918f4
gensosanso <gensosanso@gmail.com>
2022-11-10 15:42:42 +0100

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../styles/dashboard.css";
+import SingleCard from "../components/reuseable/SingleCard";
+
+import MileChart from "../charts/MileChart";
+import CarStatsChart from "../charts/CarStatsChart";
+import RecommendCarCard from "../components/UI/RecommendCarCard";
+
+import recommendCarsData from "../assets/dummy-data/recommendCars";
+
+const carObj = {
+  title: "Nombre de biens",
+  totalNumber: 7500,
+ 
+};
+
+const tripObj = {
+  title: "Deals realisés par mois",
+  totalNumber: 1697,
+ 
+};
+
+const clientObj = {
+  title: "Clients Annuels",
+  totalNumber: "8,5k",
+ 
+};
+
+const distanceObj = {
+  title: "gestionnaires de patrimoine",
+  totalNumber: 2167,
+ 
+};
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="dashboard__wrapper">
+        <div className="dashboard__cards">
+          <SingleCard item={carObj} />
+          <SingleCard item={tripObj} />
+          <SingleCard item={clientObj} />
+          <SingleCard item={distanceObj} />
+        </div>
+
+        {/* <div className="statics">
+          <div className="stats">
+            <h3 className="stats__title">Miles Statistics</h3>
+            <MileChart />
+          </div>
+
+          <div className="stats">
+            <h3 className="stats__title">Car Statistics</h3>
+            <CarStatsChart />
+          </div>
+        </div> */}
+
+        <div className="recommend__cars-wrapper">
+          {recommendCarsData.map((item) => (
+            <RecommendCarCard item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
